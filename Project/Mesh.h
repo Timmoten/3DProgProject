4e4887d6ed959886482b34f1713d50b9a568cf6c
Eric Ståhlberg <eric.stahlberg@hotmail.com>
2016-02-12 14:14:59 +0100

    Added mesh.h, branch halted atm
    Seems as if before we start loading in OBJ we should create full lighting
    and such.

---
+++ Project/Mesh.h
@@ -0,0 +1,123 @@
+#pragma once
+#ifndef MESH_H
+#define MESH_H
+
+#include <iostream>
+#include <vector>
+
+//Shader
+#include "Shader.h"
+
+//GLEW
+//#include <GL/glew.h>
+
+//GLFW
+#include <GLFW\glfw3.h>
+
+//GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+struct Vertex {
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TexCoords;
+};
+struct Texture {
+	GLuint id;
+	std::string type;
+};
+
+class Mesh {
+public:
+	/*  Mesh Data  */
+	std::vector<Vertex> vertices;
+	std::vector<GLuint> indices;
+	std::vector<Texture> textures;
+	/*  Functions  */
+	Mesh(std::vector<Vertex> vertices, std::vector<GLuint> indices, std::vector<Texture> textures);
+	void Draw(Shader shader);
+private:
+	/*  Render data  */
+	GLuint VAO, VBO, EBO;
+	/*  Functions    */
+	void setupMesh();
+};
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<GLuint> indices, std::vector<Texture> textures)
+{
+	this->vertices = vertices;
+	this->indices = indices;
+	this->textures = textures;
+
+	this->setupMesh();
+}
+
+void Mesh::setupMesh()
+{
+	glGenVertexArrays(1, &this->VAO);
+	glGenBuffers(1, &this->VBO);
+	glGenBuffers(1, &this->EBO);
+
+	glBindVertexArray(this->VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+	glBufferData(GL_ARRAY_BUFFER, this->vertices.size() * sizeof(Vertex), &this->indices[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->indices.size() * sizeof(GLuint), &this->indices[0], GL_STATIC_DRAW);
+
+	//Vertex Positions
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)0);
+
+	//Vertex Normals
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, Normal));
+
+	//Vertex Texture coordinates
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FALSE, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, TexCoords));
+
+	glBindVertexArray(0);
+}
+
+void Mesh::Draw(Shader shader)
+{
+	GLuint diffuseNr = 1;
+	GLuint specularNr = 1;
+	
+	for (GLuint i = 0; i < this->textures.size(); i++)
+	{
+		glActiveTexture(GL_TEXTURE0 + i);
+
+		std::stringstream ss;
+		std::string number;
+		std::string name = this->textures[i].type;
+
+		if (name == "texture_diffuse")
+			ss << diffuseNr++;
+		else if (name == "texture_specular")
+			ss << specularNr++;
+		number = ss.str();
+
+		glUniform1f(glGetUniformLocation(shader.Program, ("material." + name + number).c_str()), i);
+		glBindTexture(GL_TEXTURE_2D, this->textures[i].id);
+	}
+	glUniform1f(glGetUniformLocation(shader.Program, "material.shininess"), 16.0f);
+
+	//Draw Mesh
+	glBindVertexArray(this->VAO);
+	glDrawElements(GL_TRIANGLES, this->indices.size(), GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+
+	// Always good practice to set everything back to defaults once configured.
+	for (GLuint i = 0; i < this->textures.size(); i++)
+	{
+		glActiveTexture(GL_TEXTURE0 + i);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+}
+
+#endif
